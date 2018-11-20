047cb7426474aad3231880d0d15205c9c89f5c62
EllenHerc <ellencristina125@gmail.com>
2018-11-20 21:03:42 -0200

    Alterações nos xmls, criação de novas activity e definição de values e icons

--- TinderPet/app/src/main/java/com/example/windows/tinderpet/cadastroLogin.kt
+++ TinderPet/app/src/main/java/com/example/windows/tinderpet/cadastroLogin.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.app.Activity
 
 import kotlinx.android.synthetic.main.activity_cadastro_login.*
+import android.graphics.Typeface
+
+
 
 class cadastroLogin : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_login)
+
     }
 
 }
---
+++ TinderPet/app/src/main/java/com/example/windows/tinderpet/cadastroUsuario.kt
@@ -0,0 +1,13 @@
+package com.example.windows.tinderpet
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class cadastroUsuario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
