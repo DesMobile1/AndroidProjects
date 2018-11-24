19aff4dfb40e63390d9385639f8ed0cb9fd10ccc
EllenHerc <ellencristina125@gmail.com>
2018-11-24 19:27:27 -0200

    Novas activitys e alterações em arquivos XMLs

---
+++ TinderPet/app/src/main/java/com/example/windows/tinderpet/preferenciasAdotante.kt
@@ -0,0 +1,14 @@
+package com.example.windows.tinderpet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class preferenciasAdotante : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferencias_adotante)
+
+
+    }
+}
